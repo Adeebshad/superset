f35c8bdf1fc33c8a0af5135175b276626b49e328
Adeeb Bin Islam <adeebbinislam@gmail.com>
2024-08-28 10:45:17 +0600

    🐛 Fix : Excel export issue solved

--- superset-frontend/src/dashboard/components/SliceHeaderControls/index.tsx
+++ superset-frontend/src/dashboard/components/SliceHeaderControls/index.tsx
@@ -67,6 +67,7 @@ const MENU_KEYS = {
   EXPORT_CSV: 'export_csv',
   EXPORT_FULL_CSV: 'export_full_csv',
   EXPORT_XLSX: 'export_xlsx',
+  EXPORT_CHART_XLSX: 'export_chart_xlsx',
   EXPORT_FULL_XLSX: 'export_full_xlsx',
   FORCE_REFRESH: 'force_refresh',
   FULLSCREEN: 'fullscreen',
@@ -349,11 +350,11 @@ const SliceHeaderControls = (props: SliceHeaderControlsPropsWithRouter) => {
         break;
       case MENU_KEYS.EXPORT_XLSX:
         // eslint-disable-next-line no-unused-expressions
-        exportToExcel(`
-          #chart-id-${props.slice.slice_id}`,
-          `Export-Report-${getPresentDate()}`,
-        );
-        //props.exportXLSX?.(props.slice.slice_id);
+        props.exportXLSX?.(props.slice.slice_id);
+        break;
+      case MENU_KEYS.EXPORT_CHART_XLSX:
+        // eslint-disable-next-line no-unused-expressions
+        exportToExcel(`#chart-id-${props.slice.slice_id}`,`Export-Report-${getPresentDate()}`,);
         break;
       case MENU_KEYS.DOWNLOAD_AS_IMAGE: {
         // menu closes with a delay, we need to hide it manually,
@@ -553,6 +554,15 @@ const SliceHeaderControls = (props: SliceHeaderControlsPropsWithRouter) => {
           >
             {t('Export to Excel')}
           </Menu.Item>
+          {(props.slice.viz_type === 'pivot_table_v2' || props.slice.viz_type === 'table'|| 
+            props.slice.viz_type === 'time_table' || props.slice.viz_type === 'paired_ttest') && (
+            <Menu.Item
+              key={MENU_KEYS.EXPORT_CHART_XLSX}
+              icon={<Icons.FileExcelOutlined css={dropdownIconsStyles} />}
+            >
+              {t('Export Report to Excel')}
+            </Menu.Item>
+          )}
           {isFeatureEnabled(FeatureFlag.AllowFullCsvExport) &&
             props.supersetCanCSV &&
             isTable && (
